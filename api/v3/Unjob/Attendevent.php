aa19d6af3e0f6feb2cdb3811a26780f074f4cdaa
HikerBoricua <it@unidosnow.org>
2021-04-30 01:35:38 -0400

    First viable version of Event Attendance. Returns switched and total attendance to the job log. There was no code associated w. the passphrase.

--- api/v3/Unjob/Attendevent.php
+++ api/v3/Unjob/Attendevent.php
@@ -11,8 +11,8 @@ use CRM_Unutils_ExtensionUtil as E;
  */
 function _civicrm_api3_unjob_Attendevent_spec(&$spec) {
   $spec['cron_minutes']['api.required'] = 1; //Minutes between cron ticks, this job should run always
-  $spec['waiting_type']['api.required'] = 1; //ID of event type to switch/activate and update registrations (type's title works too)
-  $spec['recording_type']['api.required'] = 1; //ID that events switch to and remain in
+  $spec['waiting_type']['api.required'] = 1; //ID of event type to switch from to take registrations (type's title works too)
+  $spec['recording_type']['api.required'] = 1; //ID that events switch to in order to take registrations, and remain in
 }
 
 /**
@@ -31,6 +31,7 @@ function civicrm_api3_unjob_Attendevent($params) {
   $window_buffer = $params['cron_minutes'] + 5; //Attendance window opens minimum 5 minutes pre-start and stays open at least one cron cycle after end
   $waiting_type = $params['waiting_type'];
   $recording_type = $params['recording_type'];
+  $job_return = []; //For the scheduled job's log
 
   //Date objects bracketing the events of interest
   //(start - buffer <= now <= end + buffer) rewritten as (start <= now + buffer AND end >= now - buffer)
@@ -39,11 +40,12 @@ function civicrm_api3_unjob_Attendevent($params) {
   $dt_from->modify("+".$window_buffer." minutes");
   $dt_to->modify("-".$window_buffer." minutes");
 
-  ob_start();
+  /*ob_start();
   printf("Dates from %s to %s\n", $dt_from->format('Y-m-d H:i:s'), $dt_to->format('Y-m-d H:i:s'));
   printf("Event type: %s\n", $waiting_type);
   print "\n";
   Civi::log()->debug(ob_get_clean());
+  */
 
   //Get events of interest
   $events = civicrm_api3('Event', 'get', [
@@ -54,11 +56,12 @@ function civicrm_api3_unjob_Attendevent($params) {
     'end_date' => ['>=' => $dt_to->format('Y-m-d H:i:s')],
   ]);
 
-  ob_start();
+  /*ob_start();
   print "Events in Window:\n";
   print_r($events);
   print "\n";
   Civi::log()->debug(ob_get_clean());
+  */
 
   //Process any events
   foreach($events['values'] as $event) {
@@ -68,7 +71,8 @@ function civicrm_api3_unjob_Attendevent($params) {
     print_r($event);
     printf ("active? %s\n", $event['is_active']);
     printf ("id? %s\n", $event['id']);
-    Civi::log()->debug(ob_get_clean());*/
+    Civi::log()->debug(ob_get_clean());
+    */
 
     //If the event is type "waiting" waiting, start "recording"" attendance webforms
     if ($event['event_type_id'] == $waiting_type) {
@@ -82,81 +86,76 @@ function civicrm_api3_unjob_Attendevent($params) {
     $switched = registered_to_attended($event['id'],
       date_create($event['start_date'])->modify("-".$window_buffer." minutes")->format('Y-m-d H:i:s'),
       $event['end_date']);
+    $job_return[$event['id']]['switched'] = $switched;
 
+    //Count event's attendance
+    $attended = count_attendance($event['id']);
+    $job_return[$event['id']]['attended'] = $attended;
   }
 
-  $returnValues = [];
-  return civicrm_api3_create_success($returnValues, $params, 'Unjob', 'Attendevent');
+  return civicrm_api3_create_success($job_return, $params, 'Unjob', 'Attendevent');
 }
 
-function registered_to_attended($event, $from, $to) {
+function registered_to_attended($event_id, $from, $to) {
   //Change participant status from registered to attended if record created within the event's time window
-  ob_start();
-  printf("Event: %s\n", $event);
+
+  /*ob_start();
+  printf("Event: %s\n", $event_id);
   printf("Dates from %s to %s\n", $from, $to);
   Civi::log()->debug(ob_get_clean());
+  */
 
   $registereds = civicrm_api3('Participant', 'get', [
     'sequential' => 1,
-    'event_id' => $event,
+    'event_id' => $event_id,
     'status_id' => 'Registered',
     'register_date' => ['>=' => $from],
     // 'register_date' => ['<=' => $to], BETWEEN failed on syntax and adding this killed all register_date filtering
   ]);
 
-  ob_start();
+  /*ob_start();
   print "Registered:\n";
   print_r($registereds);
   print "\n";
   Civi::log()->debug(ob_get_clean());
+  */
   
+  $switched = 0;
   foreach($registereds['values'] as $registered) {
     if (strtotime($registered['participant_register_date']) > strtotime($to)) continue; //Other half of the misfiring BETWEEN in api3
     civicrm_api3('Participant', 'create', [
       'id' => $registered['id'],
       'status_id' => "Attended",
     ]);
+    $switched += 1;
 
-    ob_start();
+    /*ob_start();
     printf("%s to attended\n", $registered['display_name']);
     printf("Comparison %d > %d\n", strtotime($registered['participant_register_date']), strtotime($to));
     Civi::log()->debug(ob_get_clean());
+    */
   }
-
+  return $switched;
 }
 
+function count_attendance($event_id) {
+  //Fill the corresponding event custom field with the number of individuals in "Attended" participant records
+  $attended = 0;
 
+  $attendees = civicrm_api3('Participant', 'get', [ //Not limiting count to particular roles in the event
+    'sequential' => 1,
+    'event_id' => $event_id,
+    'status_id' => 'Attended',
+  ]);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  /*if (array_key_exists('magicword', $params) && $params['magicword'] == 'sesame') {
-    $returnValues = array(
-      // OK, return several data rows
-      12 => ['id' => 12, 'name' => 'Twelve'],
-      34 => ['id' => 34, 'name' => 'Thirty four'],
-      56 => ['id' => 56, 'name' => 'Fifty six'],
-    );
-    // ALTERNATIVE: $returnValues = []; // OK, success
-    // ALTERNATIVE: $returnValues = ["Some value"]; // OK, return a single value
-
-    // Spec: civicrm_api3_create_success($values = 1, $params = [], $entity = NULL, $action = NULL)
-    return civicrm_api3_create_success($returnValues, $params, 'Unjob', 'Attendevent');
+  foreach($attendees['values'] as $attendee) {
+    $attended += 1;
+    if ($attendee['custom_171'] > 0) $attended += $attendee['custom_171']; //Additional attendees
   }
-  else {
-    throw new API_Exception('Everyone knows that the magicword is "sesame"', 'magicword_incorrect');
-  } */
 
+  civicrm_api3('Event', 'create', [
+    'id' => 160,
+    'custom_129' => $attended, //Total event attendance, will be updated throughout the event's time window
+  ]);
+  return $attended;
+}
