e39a8d7922dd53931884393480b41790cb93a285
HikerBoricua <38139762+HikerBoricua@users.noreply.github.com>
2023-10-17 19:47:07 -0400

    group_match
    If the Event has a group for expected attendees, take extra measures when a contact registers but is not in the group. Goal is to match even if the attendance form isn't filled just right.

--- api/v3/Unjob/Attendevent.php
+++ api/v3/Unjob/Attendevent.php
@@ -54,6 +54,7 @@ function civicrm_api3_unjob_Attendevent($params) {
     'event_type_id' => ['IN' => [$waiting_type, $recording_type]],
     'start_date' => ['<=' => $dt_from->format('Y-m-d H:i:s')],
     'end_date' => ['>=' => $dt_to->format('Y-m-d H:i:s')],
+    'options' => ['limit' => 0]
   ]);
 
   /*ob_start();
@@ -66,15 +67,13 @@ function civicrm_api3_unjob_Attendevent($params) {
   //Process any events
   foreach($events['values'] as $event) {
 
-    /*ob_start();
+ob_start();
     print "Current Event:\n";
     print_r($event);
-    printf ("active? %s\n", $event['is_active']);
-    printf ("id? %s\n", $event['id']);
-    Civi::log()->debug(ob_get_clean());
-    */
+Civi::log()->debug(ob_get_clean());
+    
 
-    //If the event is type "waiting" waiting, start "recording"" attendance webforms
+    //If the event is type "waiting" start "recording" attendance webforms
     if ($event['event_type_id'] == $waiting_type) {
       $result = civicrm_api3('Event', 'create', [
         'id' => $event['id'],
@@ -83,7 +82,7 @@ function civicrm_api3_unjob_Attendevent($params) {
     }
 
     //Process event's participant records registered between (start - buffer) and end
-    $switched = registered_to_attended($event['id'],
+    $switched = registered_to_attended($event,
       date_create($event['start_date'])->modify("-".$window_buffer." minutes")->format('Y-m-d H:i:s'),
       $event['end_date']);
     $job_return[$event['id']]['switched'] = $switched;
@@ -96,7 +95,7 @@ function civicrm_api3_unjob_Attendevent($params) {
   return civicrm_api3_create_success($job_return, $params, 'Unjob', 'Attendevent');
 }
 
-function registered_to_attended($event_id, $from, $to) {
+function registered_to_attended($event, $from, $to) {
   //Change participant status from registered to attended if record created within the event's time window
 
   /*ob_start();
@@ -106,23 +105,23 @@ function registered_to_attended($event_id, $from, $to) {
   */
 
   $registereds = civicrm_api3('Participant', 'get', [
-    'sequential' => 1,
-    'event_id' => $event_id,
+    'sequential' => 1, //To access values[0] later
+    'event_id' => $event['id'],
     'status_id' => 'Registered',
     'custom_173' => ['>=' => $from], //Custom field where attendance form populates submission datetime as a string
   ]);
 
-  /*ob_start();
+ob_start();
   print "Registered:\n";
   print_r($registereds);
   print "\n";
-  Civi::log()->debug(ob_get_clean());
-  */
+Civi::log()->debug(ob_get_clean());
+  
   
   $new_switched = 0;
   $all_switched = civicrm_api3('Event', 'get', [
     'sequential' => 1,
-    'id' => $event_id,
+    'id' => $event['id'],
     'return' => ["custom_170"], //Get webform submissions from previous runs to add this run
     ])['values'][0]['custom_170'] ?? 0;
 
@@ -135,9 +134,9 @@ function registered_to_attended($event_id, $from, $to) {
   
   foreach($registereds['values'] as $registered) {
     if (strtotime($registered['custom_173']) > strtotime($to)) continue; //Instead of the misfiring use of BETWEEN in the participant get
-    civicrm_api3('Participant', 'create', [
-      'id' => $registered['id'],
-      'status_id' => "Attended",
+      civicrm_api3('Participant', 'create', [
+        'id' => $registered['id'],
+        'status_id' => "Attended",
     ]);
     $new_switched += 1;
 
@@ -146,11 +145,15 @@ function registered_to_attended($event_id, $from, $to) {
     printf("Comparison %d > %d\n", strtotime($registered['participant_register_date']), strtotime($to));
     Civi::log()->debug(ob_get_clean());
     */
+
+    //Evaluate group membership
+    group_match($event, $registered);
+
   }
 
   $all_switched += $new_switched;
   civicrm_api3('Event', 'create', [
-    'id' => $event_id,
+    'id' => $event['id'],
     'custom_170' => $all_switched, //Save the new count of webforms submitted
   ]);
 
@@ -165,6 +168,7 @@ function count_attendance($event_id) {
     'sequential' => 1,
     'event_id' => $event_id,
     'status_id' => 'Attended',
+    'options' => ['limit' => 0]
   ]);
 
   foreach($attendees['values'] as $attendee) {
@@ -177,4 +181,135 @@ function count_attendance($event_id) {
     'custom_129' => $attended, //Total event attendance, will be updated throughout the event's time window
   ]);
   return $attended;
+}
+
+function group_match($event, $registered) {
+  //If the event specifies a valid group of anticipated attendees, try and match this attendee to a member of the group
+
+  if(($group_id = intval($event['custom_204'])) <= 0)
+    return; //Group ID custom field
+
+ob_start();
+printf("Group id = %s\n", $group_id);
+Civi::log()->debug(ob_get_clean());
+
+  $groupContacts = civicrm_api3('GroupContact', 'get', [
+    'sequential' => 1, //To access values[0] later
+    'group_id' => $group_id,
+    'options' => ['limit' => 0]
+  ]);
+
+  if(!is_array($groupContacts)) //Ignore if failed to get a result
+    return;
+
+  if($groupContacts['count'] == 0) //Ignore if the group is empty
+    return;
+
+  //Create an array indexed by contact id
+  $contactsGroup = [];
+  foreach ($groupContacts['values'] as $groupContact) {
+    $contactsGroup[$groupContact['contact_id']] = $groupContact;
+  }
+
+ob_start();
+  echo("contactsGroup\n");
+  print_r($contactsGroup);
+Civi::log()->debug(ob_get_clean());
+
+  //Is the registered contact in the array? If so nothing to do, all's good
+  if(array_key_exists($registered['contact_id'], $contactsGroup))
+    return;
+    
+ob_start();
+  echo("Registered not in group\n");
+Civi::log()->debug(ob_get_clean());
+
+  //Get registered's phone and email
+  $phone_email = civicrm_api3('Contact', 'get', [
+    'sequential' => 1,
+    'return' => ["email", "phone"],
+    'id' => $registered['contact_id'],
+  ]);
+
+  //Find contacts that match registered by either email or phone
+  $matches = \Civi\Api4\Contact::getDuplicates()
+   ->setDedupeRule('Phone_or_Email')
+   ->addValue('phone_primary.phone_numeric', $phone_email['values'][0]['phone'])
+   ->addValue('email_primary.email', $phone_email['values'][0]['email'])
+   ->execute();
+ob_start();
+echo("Registered phone_email\n");
+print_r($phone_email);
+printf("%s matches\n", $matches->count());
+print_r($matches);
+Civi::log()->debug(ob_get_clean());
+
+  //Only interested in matches in the group
+  $groupMatch = [];
+  if($matches->count() > 1) { //Should always match at least registered
+    foreach ($matches as $match) {
+      if(array_key_exists($match['id'], $contactsGroup))
+        $groupMatch[$match['id']] = $contactsGroup[$match['id']];
+    }
+ob_start();  
+    echo("groupMatch\n");
+    print_r($groupMatch);
+Civi::log()->debug(ob_get_clean());
+  }
+  
+  $activityParams = ['values' => [
+  'activity_type_id' => 1, //14 = Follow-up, 1 = Meeting
+  'status_id' => 7, //Available
+  'assignee_contact_id' => [$event['created_id']],
+  'source_contact_id' => $registered['contact_id'],
+  'target_contact_id' => array_keys($groupMatch),
+  'subject' => 'Unexpected attendance',
+  'details' => sprintf("Event: %s<br>\nAttendance form from %s (id %s)<br>\nContact is not in the event's group of \"Anticipated participants\" (id %s)<br>\nAction taken: ",
+    $event['title'], $registered['display_name'],
+    $registered['contact_id'], $groupContacts['values'][0]['group_id']), 
+  ]];
+
+ob_start();  
+    echo("activityParams\n");
+    print_r($activityParams);
+Civi::log()->debug(ob_get_clean());
+
+  $newActivity = NULL;
+
+  if(count($groupMatch) == 0) {
+    $activityParams['values']['details'] .= "No action taken, didn't match any contacts in the group<br>\n";
+    $newActivity = civicrm_api4('Activity', 'create', $activityParams);
+
+  } elseif (count($groupMatch) > 1) {
+    $activityParams['values']['details'] .= "No action taken, matched more than one \"With Contact(s)\" in the group<br>\n";
+    $newActivity = civicrm_api4('Activity', 'create', $activityParams);
+
+  } else {
+  //One match in group
+  //Reassign the participant record to this match
+  $participantParams = [
+    'where' => [['id', '=', $registered['id']]],
+    'values' => ['contact_id' => array_keys($groupMatch)[0]]
+  ];
+
+ob_start();  
+  echo("participantParams\n");
+  print_r($participantParams);
+Civi::log()->debug(ob_get_clean());
+
+  civicrm_api4('Participant', 'update', $participantParams);
+  $activityParams['values']['details'] .= "Attendance reassigned to the matching \"With Contact(s)\" in the group<br>\n";
+  $newActivity = civicrm_api4('Activity', 'create', $activityParams);
+  }
+  
+  if(isset($newActivity)) {
+//APIs don't send emails to assignees, force one
+        $assignee_contact = civicrm_api3('Contact', 'getsingle', [
+          'id' => $activityParams['values']['assignee_contact_id'],
+        ]);
+
+        CRM_Case_BAO_Case::sendActivityCopy(NULL, $newActivity->single()['id'], [$assignee_contact['email'] => $assignee_contact], NULL, NULL);
+
+  }
+
 }
